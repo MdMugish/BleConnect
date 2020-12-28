eeabecc8d75a6e2cca82ff886f547432d61e0b6c
Mugish <mohammad.mugish@gmail.com>
2020-12-28 15:22:33 +0530

    Discover, Read,Write,Notify values on peripheral

--- BleConnect/BleConnect/BluetoothManager.swift
+++ BleConnect/BleConnect/BluetoothManager.swift
@@ -11,23 +11,59 @@ import CoreBluetooth
 
 class BluetoothManager : ObservableObject  {
 
-    @Published var bluetoothState : BluetoothState = .unknown
+    @Published var bluetoothState : BluetoothState = .unknown{
+        didSet{
+            if bluetoothState  == .connected{
+                connectedDevice = bleConnect.connectedDevice()
+            }
+        }
+    }
+    
     @Published var listOfDevice = [CBPeripheral]()
+    var connectedDevice : CBPeripheral?
+    
     var bleConnect: BleConnect!
 
     init() {
         bleConnect = BleConnect(delegate: self, deviceNameShouldContain: nil, deviceUUID: nil)
+        
     }
 
 }
 
 extension BluetoothManager : BLE_Callbacks{
+
+
     
     func didBluetoothStateChange(_ bluetoothState: BluetoothState) {
         self.bluetoothState = bluetoothState
     }
+    
 
      func didDiscoverDevices(_ listOfDevice: [CBPeripheral]) {
         self.listOfDevice = listOfDevice
     }
+    
+    func didDiscoverCharacteristicsForService(_ service: CBService, allCharacterisricsForThisService: [CBCharacteristic]) {
+        print("Service : \(service) and All characteristics for this service is \(allCharacterisricsForThisService)")
+    }
+    
+    func didUpdateNotificationForCharacteristics(characteristics: CBCharacteristic, error: Error?) {
+        
+    }
+
+    
+    func didUpdateValueForCharacteristics(characteristics: CBCharacteristic, error: Error?) {
+        
+    }
+    
+    func didWriteValueForCharacteristics(characteristics: CBCharacteristic, error: Error?) {
+        
+    }
+    
+    func didUpdateANCSAuthorized(state: Bool) {
+        
+    }
+    
+   
 }
